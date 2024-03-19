172c03c7c9808f0c81788707380046b54fd9833b
boris <boristskhay@gmail.com>
2024-03-19 22:27:50 +0900

    basic structure

---
+++ front/src/Components/MainPage/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MainPage = () => {
+    return (
+        <>
+            <h1 className="text-3xl caret-lime-300">
+                Hello world
+            </h1>
+        </>
+    )
+}
+
+export default MainPage;
